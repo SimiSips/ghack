968354dc99c974b7c4e58e52db9f4f4ab0780be9
TheRealKutlwano <tshatiwakb@gmail.com>
2021-03-13 00:24:39 +0200

    Update views.py

--- deephospital/app/views.py
+++ deephospital/app/views.py
@@ -1,3 +1,8 @@
+# Import necessary libaries
+from tensorflow.keras.models import load_model
+import cv2 
+import numpy as np 
 from django.shortcuts import render
 
-
+# Load the machine learning model
+model = load_model("deephospital_model")
