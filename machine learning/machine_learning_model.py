88f19a413847a7e80d0feb10584e3124aa2c1442
TheRealKutlwano <tshatiwakb@gmail.com>
2021-03-12 20:59:53 +0200

    Update machine_learning_model.py

--- machine learning/machine_learning_model.py
+++ machine learning/machine_learning_model.py
@@ -2,7 +2,7 @@
 import tensorflow as tf 
 from tensorflow.keras.preprocessing import ImageDataGenerator
 import matplotlib.pyplot as plt 
-import splitfolder
+import splitfolders
 from tensorflow.keras.callbacks import TensorBoard
 import time 
 
@@ -11,3 +11,4 @@ NAME = "DeepHospitalCNN-{}".format(int(time.time()))
 tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
 
 # Use split folders library to split into train and test set
+splitfolders.ratio("dataset", "new_data", ratio=(0.9, 0.1))
